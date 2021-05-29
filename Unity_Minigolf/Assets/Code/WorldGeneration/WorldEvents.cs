22196337f3f5e1aa9e53f24aa06eb97ca3eaf180
Maximilian Häming <Maximilian@haeming.de>
2021-05-29 10:22:50 +0200

    rfct: world control via event system

---
+++ Unity_Minigolf/Assets/Code/WorldGeneration/WorldEvents.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldEvents : MonoBehaviour
+{
+    /// <summary>
+    /// Generate a series of base blocks to ease the player into the level
+    /// </summary>
+    public void startEvent()
+    {
+        
+    }
+
+    
+    /// <summary>
+    /// Transition into base blocks with an ending sequence
+    /// </summary>
+    public void endEvent()
+    {
+        
+    }
+    
+    
+    /// <summary>
+    /// Reset the player and stage after hitting an obstacle
+    /// </summary>
+    public void resetEvent()
+    {
+        
+    }
+}
--- Unity_Minigolf/Assets/Code/WorldGeneration/WorldGenerator.cs
+++ Unity_Minigolf/Assets/Code/WorldGeneration/WorldGenerator.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Object = System.Object;
 using Random = UnityEngine.Random;
 
 /// <summary>
@@ -16,23 +15,13 @@ public class WorldGenerator : MonoBehaviour
     [Header("Game Objects")]
     // Player
     public GameObject playerobj;
-
-    [Header("Time Settings")]
-    // Time
-    public float timeLimit = 60;
-    // Fade-In-Time
-    [HideInInspector]
-    public float fadeInTime = 5;
-    // Fade-Out-Time 
-    [HideInInspector]
-    public float fadeOutTime = 5;
-
+    
     [Header("Generation Settings")]
     // How many blocks should be kept behind the Player
     public int keepFloorElements = 2;
 
-    public float pitFreq;
-    public float obstacleFreq;
+    private float pitFreq;
+    private float obstacleFreq;
 
     private float _activeFloorPositionZ;
     private float _activeFloorPositionY;
@@ -42,60 +31,54 @@ public class WorldGenerator : MonoBehaviour
     private List<GameObject> activePits = new List<GameObject>();
     private List<GameObject> activeObstacles = new List<GameObject>();
 
+
+    public void Update()
+    {
+        GenerateWorld();
+        // If there are no objects available from the pool, go through all active Floor objects in the scene and check if you can deactivate some of them
+        ClearBehind(activeFloors);
+        ClearBehind(activePits);
+        ClearBehind(activeObstacles);
+    }
     
     // Update is called once per frame
-    void Update()
+    public void GenerateWorld()
     {
-        if (timeLimit > 0)
+        if (Random.value < pitFreq)
         {
-            if (Random.value < pitFreq)
+            GameObject pit = ObjectPool.sharedInstance.GetPooledObject("Pit");
+            if (pit)
             {
-                GameObject pit = ObjectPool.sharedInstance.GetPooledObject("Pit");
-                if (pit != null)
-                {
-                    placeBlock(pit);
-                    activePits.Add(pit);
-                }
+                PlaceBlock(pit);
+                activePits.Add(pit);
             }
-            else
+        }
+        else
+        {
+            // Retrieve floor objects from pool
+            GameObject floor = ObjectPool.sharedInstance.GetRandomPoolObject("Floor");
+            if (floor)
             {
-                // Retrieve floor objects from pool
-                GameObject floor = ObjectPool.sharedInstance.GetRandomPoolObject("Floor");
-                if (floor)
+                PlaceBlock(floor);
+                activeFloors.Add(floor);
+                if (Random.value < obstacleFreq)
                 {
-                    placeBlock(floor);
-                    activeFloors.Add(floor);
-                    if (Random.value < obstacleFreq)
+                    GameObject obstacle = ObjectPool.sharedInstance.GetPooledObject("Obstacle");
+                    if (obstacle)
                     {
-                        GameObject obstacle = ObjectPool.sharedInstance.GetPooledObject("Obstacle");
-                        if (obstacle)
-                        {
-                            placeObstacle(obstacle);
-                            activeObstacles.Add(obstacle);
-                        }
+                        PlaceObstacle(obstacle);
+                        activeObstacles.Add(obstacle);
                     }
                 }
             }
-            
-            // If there are no objects available from the pool, go through all active Floor objects in the scene and check if you can deactivate some of them
-            clearBehind(activeFloors);
-            clearBehind(activePits);
-            clearBehind(activeObstacles);
-
-
         }
-        
     }
-
-    private void FixedUpdate()
-    {
-        timeLimit -= Time.deltaTime;
-    }
-
+    
+    
     /// <summary>
     /// Place floor elements relative to the player position
     /// </summary>
-    public void placeBlock(GameObject block)
+    private void PlaceBlock(GameObject block)
     {
         // Place objects in front of the Player
         block.transform.position = new Vector3(_activeFloorPositionX, _activeFloorPositionY, _activeFloorPositionZ);
@@ -111,31 +94,14 @@ public class WorldGenerator : MonoBehaviour
         // _activeFloorPositionX;
     }
 
-    public void placeObstacle(GameObject obstacle)
+    private void PlaceObstacle(GameObject obstacle)
     {
         int lane = (int)((2 * Random.value - 1) + _activeFloorPositionX);
         obstacle.transform.position = new Vector3(lane, _activeFloorPositionY + obstacle.transform.lossyScale.y, _activeFloorPositionZ);
         obstacle.SetActive(true);
     }
-
-    /// <summary>
-    /// Generate a series of base blocks to ease the player into the level
-    /// </summary>
-    public void startEvent()
-    {
-        
-    }
-
     
-    /// <summary>
-    /// Transition into base blocks with an ending sequence
-    /// </summary>
-    public void endEvent()
-    {
-        
-    }
-    
-    public void clearBehind(List<GameObject> list)
+    protected void ClearBehind(List<GameObject> list)
     {
         for (int i = 0; i < list.Count; i++)
         {
@@ -144,5 +110,62 @@ public class WorldGenerator : MonoBehaviour
                 list[i].SetActive(false);
             }
         }
+    }
+
+    protected void ClearAll()
+    {
+        for (int i = 0; i < activeFloors.Count; i++)
+        {
+            activeFloors[i].SetActive(false);
+            activeFloors.RemoveAt(i);
+        }
+
+        for (int i = 0; i < activeObstacles.Count; i++)
+        {
+            activeObstacles[i].SetActive(false);
+            activeObstacles.RemoveAt(i);
+        }
+        
+        for (int i = 0; i < activePits.Count; i++)
+        {
+            activePits[i].SetActive(false);
+            activePits.RemoveAt(i);
+        }
+    }
+    
+    
+
+    public float GetObstacleFreq()
+    {
+        return obstacleFreq;
+    }
+    
+    public void SetObstacleFreq(float freq)
+    {
+        obstacleFreq = freq;
+    }
+
+    public float GetPitFreq()
+    {
+        return pitFreq;
+    }
+
+    public void SetPitFreq(float freq)
+    {
+        pitFreq = freq;
+    }
+
+    public List<GameObject> GetActiveFloors()
+    {
+        return activeFloors;
+    }
+    
+    public List<GameObject> GetActiveObstacles()
+    {
+        return activeObstacles;
+    }
+    public List<GameObject> GetActivePits()
+    {
+        return activePits;
     }
 }
