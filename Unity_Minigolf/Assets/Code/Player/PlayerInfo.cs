af4204eac87c070fc04f747ce9e61e64dcaf59a7
Maximilian Häming <Maximilian@haeming.de>
2021-06-30 14:29:09 +0200

    wip: draft for distance to next obstacle

--- Unity_Minigolf/Assets/Code/Player/PlayerInfo.cs
+++ Unity_Minigolf/Assets/Code/Player/PlayerInfo.cs
@@ -1,4 +1,5 @@
 using System;
+using Code.World;
 using UnityEngine;
 
 namespace Code.Player
@@ -7,10 +8,16 @@ namespace Code.Player
     {
 
         public static PlayerInfo info;
-
+        
         public int HitObstacles { get; private set; }
         public int HitPits { get; private set; }
         
+        public Vector3 ClosestObstacleAt { get; private set; }
+        public Vector3 ClosestPitAt { get; private set; }
+        public float DistanceToNextObstacle { get; private set; }
+        public float DistanceToNextPit { get; private set; }
+        public float DistanceToNextObject { get; private set; }
+
         private void Awake()
         {
             // Destroy any other existing Player Info
@@ -28,12 +35,16 @@ namespace Code.Player
         {
             PlayerBehavior.HitObstacle += OnObstacleHit;
             PlayerBehavior.HitPit += OnPitHit;
+            WorldGenerator.ObstaclePlaced += OnNewCloseObstacle;
+            WorldGenerator.PitPlaced += OnNewClosePit;
         }
 
         private void OnDisable()
         {
             PlayerBehavior.HitObstacle -= OnObstacleHit;
             PlayerBehavior.HitPit -= OnPitHit;
+            WorldInfo.NewCloseObstacle -= OnNewCloseObstacle;
+            WorldInfo.NewClosePit -= OnNewClosePit;
         }
 
         private void OnObstacleHit()
@@ -47,7 +58,31 @@ namespace Code.Player
             HitPits++;
             Debug.Log("Pits hit: " + HitPits);
         }
+
+        private void OnNewCloseObstacle(Vector3 pos)
+        {
+            float dist = Vector3.Distance(pos, transform.position);
+            
+            if (DistanceToNextObstacle < DistanceToNextObject)
+            {
+                DistanceToNextObject = DistanceToNextObstacle;
+                Debug.Log("Closest Object(Obstacle) at: " + DistanceToNextObject);
+            }
+        }
         
+        private void OnNewClosePit(Vector3 pos)
+        {
+            DistanceToNextPit = Vector3.Distance(pos, transform.position);
+            if (DistanceToNextPit < DistanceToNextObject)
+            {
+                DistanceToNextObject = DistanceToNextPit;
+                Debug.Log("Closest Object(Pit) at:" + DistanceToNextObject);
+
+            }
+        }
+        
+        
+
         // returns in which lane the player is at that moment (left=1, middle=2, right=3)
         public int GetLane()
         {
