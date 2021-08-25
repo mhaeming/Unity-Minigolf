60a7225ddb929789578fdd03333ebc6cea8b9585
KrisKob <49274795+KrisKob@users.noreply.github.com>
2021-08-25 13:08:55 +0200

    adjusted Experiment namespace

--- Unity_Minigolf/Assets/Code/Experiment/DataCollection.cs
+++ Unity_Minigolf/Assets/Code/Experiment/DataCollection.cs
@@ -1,79 +1,76 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
 using Code.Player;
-using Code.World;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class DataCollection : MonoBehaviour
+namespace Code.Experiment
 {
-
-    public Statistics localData = new Statistics();
-
-    void Start()
+    public class DataCollection : MonoBehaviour
     {
-        localData = ExperimentManager.Instance.savedData;
-    }
 
-    private void OnDisable()
-    {
-        if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Tutorial)
+        public Statistics localData = new Statistics();
+
+        void Start()
         {
-            //CharacterEditor:
-            // get time spend in character editor from FullSceneManager
-            localData.time = FullSceneManager.timeCustomize;
-            Debug.Log("time: " + localData.time);
+            localData = ExperimentManager.Instance.savedData;
+        }
 
-            if (ExperimentManager.Instance.savedData.isDecision)
+        private void OnDisable()
+        {
+            if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Tutorial)
             {
-                // get both items and interactions from CharacterCustomization
-                //items: different characters that were clicked on
-                localData.items = CharacterCustomization.items;
+                //CharacterEditor:
+                // get time spend in character editor from FullSceneManager
+                localData.time = FullSceneManager.timeCustomize;
+                Debug.Log("time: " + localData.time);
 
-                //interactions: total number of clicks in editor
-                localData.interactions = CharacterCustomization.interactions;
+                if (ExperimentManager.Instance.savedData.isDecision)
+                {
+                    // get both items and interactions from CharacterCustomization
+                    //items: different characters that were clicked on
+                    localData.items = CharacterCustomization.items;
+
+                    //interactions: total number of clicks in editor
+                    localData.interactions = CharacterCustomization.interactions;
+                }
+                else
+                {
+                    localData.items = 0;
+                }
+                Debug.Log("items: " + localData.items + ", interactions: " + localData.interactions);
             }
-            else
+
+            if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Feedback)
             {
-                localData.items = 0;
+                //get information on the main scene from PlayerInfo:
+                localData.metres = PlayerInfo.DistanceTraveled;
+                localData.failures = PlayerInfo.HitObstacles + PlayerInfo.HitPits;
+                Debug.Log("metres: " + localData.metres + ", failures: " + localData.failures);
+                //levels t.b. implemented in WorldEvents class
             }
-            Debug.Log("items: " + localData.items + ", interactions: " + localData.interactions);
+            SaveStatistics();
         }
 
-        if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Feedback)
+        public void SaveStatistics()
         {
-            //get information on the main scene from PlayerInfo:
-            localData.metres = PlayerInfo.DistanceTraveled;
-            localData.failures = PlayerInfo.HitObstacles + PlayerInfo.HitPits;
-            Debug.Log("metres: " + localData.metres + ", failures: " + localData.failures);
-            //levels t.b. implemented in WorldEvents class
-        }
-        SaveStatistics();
-    }
-
-    public void SaveStatistics()
-    {
-        // handle when which data is saved to make sure that no data is overwritten accidentally:
-        // save time, items and interactions after Customize scene
-        if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Tutorial)
-        {
-            ExperimentManager.Instance.savedData.time = localData.time;
-            ExperimentManager.Instance.savedData.items = localData.items;
-            ExperimentManager.Instance.savedData.interactions = localData.interactions;
-        }
-        // save metres & failures (& levels) after Play scene
-        if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Feedback)
-        {
-            ExperimentManager.Instance.savedData.metres = localData.metres;
-            ExperimentManager.Instance.savedData.failures = localData.failures;
-            //ExperimentManager.Instance.savedData.levels = localData.levels;
-        }
-        // set bool dataCollected = true when all data is collected
-        if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.End)
-        {
-            ExperimentManager.Instance.savedData.dataCollected = true;
+            // handle when which data is saved to make sure that no data is overwritten accidentally:
+            // save time, items and interactions after Customize scene
+            if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Tutorial)
+            {
+                ExperimentManager.Instance.savedData.time = localData.time;
+                ExperimentManager.Instance.savedData.items = localData.items;
+                ExperimentManager.Instance.savedData.interactions = localData.interactions;
+            }
+            // save metres & failures (& levels) after Play scene
+            if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Feedback)
+            {
+                ExperimentManager.Instance.savedData.metres = localData.metres;
+                ExperimentManager.Instance.savedData.failures = localData.failures;
+                //ExperimentManager.Instance.savedData.levels = localData.levels;
+            }
+            // set bool dataCollected = true when all data is collected
+            if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.End)
+            {
+                ExperimentManager.Instance.savedData.dataCollected = true;
+            }
         }
     }
 }
--- Unity_Minigolf/Assets/Code/Experiment/ExperimentManager.cs
+++ Unity_Minigolf/Assets/Code/Experiment/ExperimentManager.cs
@@ -1,134 +1,134 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using Code.CSV;
-using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class ExperimentManager : MonoBehaviour
+namespace Code.Experiment
 {
-    private int _prob;
-    private string _fileAddress;
+    public class ExperimentManager : MonoBehaviour
+    {
+        private int _prob;
+        private string _fileAddress;
     
-    public Statistics savedData = new Statistics();
+        public Statistics savedData = new Statistics();
     
-    // make sure that the data saved here won't be deleted when transitioning between scenes:
-    public static ExperimentManager Instance;
-    void Awake ()   
-    {
-        if (Instance == null)
+        // make sure that the data saved here won't be deleted when transitioning between scenes:
+        public static ExperimentManager Instance;
+        void Awake ()   
         {
-            DontDestroyOnLoad(gameObject);
-            Instance = this;
-        }
-        else if (Instance != this)
-        {
-            Destroy (gameObject);
-        }
-
-        // handle which of the two groups the trial belongs to
-        if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Start)
-        {
-            // randomly choose one of the two groups (experimental or control)
-            _prob = Random.Range(0, 2);
-
-            if (_prob == 0)
+            if (Instance == null)
             {
-                savedData.isDecision = false;
-                Debug.Log("You've landed in the control group.");
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
             }
-            else
+            else if (Instance != this)
             {
-                savedData.isDecision = true;
-                Debug.Log("You've landed in the experimental group.");
-            }   
-        }
-    }
-
-    private void Start()
-    {
-        _fileAddress = Application.dataPath + "/Assets/CSVData/data.csv";
-        _fileAddress.ToString();
-    }
-
-    private void OnDisable()
-    {
-        if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.End)
-        {
-            defineCsvHeader();
-            
-            // only create the csv line when the data has been collected and saved to savedData Statistics
-            if (savedData.dataCollected)
-            {
-                CreateCsvLine();
+                Destroy (gameObject);
             }
-        
-            // only create the csv file when the data has been collected and a csv file has not yet been created
-            if (savedData.dataCollected && !savedData.csvCreated)
+
+            // handle which of the two groups the trial belongs to
+            if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.Start)
             {
-                /*List<string> csv = CSVTools.CreateCsv(savedData.csvData, savedData.csvHeader);
-                CSVTools.SaveCsv(csv, Application.dataPath + "/Assets/CSVData/data" + GUID.Generate());
-                Debug.Log("Created CSV file.");
-                savedData.csvCreated = true;*/
-                if (File.Exists(_fileAddress))
+                // randomly choose one of the two groups (experimental or control)
+                _prob = Random.Range(0, 2);
+
+                if (_prob == 0)
                 {
-                    Debug.Log("UpdateCsv");
-                    CSVTools.UpdateCsv(savedData.csvData, _fileAddress);
+                    savedData.isDecision = false;
+                    Debug.Log("You've landed in the control group.");
                 }
                 else
                 {
-                    CSVTools.CreateEmptyCsv(savedData.csvHeader, _fileAddress);
-                    Debug.Log("UpdateCsv");
-                    CSVTools.UpdateCsv(savedData.csvData, _fileAddress);
+                    savedData.isDecision = true;
+                    Debug.Log("You've landed in the experimental group.");
+                }   
+            }
+        }
+
+        private void Start()
+        {
+            _fileAddress = Application.dataPath + "/Assets/CSVData/data.csv";
+            _fileAddress.ToString();
+        }
+
+        private void OnDisable()
+        {
+            if (FullSceneManager.CurrentScene == FullSceneManager.sceneEnum.End)
+            {
+                defineCsvHeader();
+            
+                // only create the csv line when the data has been collected and saved to savedData Statistics
+                if (savedData.dataCollected)
+                {
+                    CreateCsvLine();
+                }
+        
+                // only create the csv file when the data has been collected and a csv file has not yet been created
+                if (savedData.dataCollected && !savedData.csvCreated)
+                {
+                    /*List<string> csv = CSVTools.CreateCsv(savedData.csvData, savedData.csvHeader);
+                CSVTools.SaveCsv(csv, Application.dataPath + "/Assets/CSVData/data" + GUID.Generate());
+                Debug.Log("Created CSV file.");
+                savedData.csvCreated = true;*/
+                    if (File.Exists(_fileAddress))
+                    {
+                        Debug.Log("UpdateCsv");
+                        CSVTools.UpdateCsv(savedData.csvData, _fileAddress);
+                    }
+                    else
+                    {
+                        CSVTools.CreateEmptyCsv(savedData.csvHeader, _fileAddress);
+                        Debug.Log("UpdateCsv");
+                        CSVTools.UpdateCsv(savedData.csvData, _fileAddress);
+                    }
                 }
             }
         }
-    }
     
 
-    private void defineCsvHeader()
-    {
-        //savedData.csvHeader.Add("subjectNR");
-        //savedData.csvHeader.Add("trialNR");
-        savedData.csvHeader.Add("isDecision");
-        savedData.csvHeader.Add("time");
-        savedData.csvHeader.Add("items");
-        savedData.csvHeader.Add("interactions");
-        savedData.csvHeader.Add("metres");
-        savedData.csvHeader.Add("failures");
-        //savedData.csvHeader.Add("levels");
-    }
+        private void defineCsvHeader()
+        {
+            //savedData.csvHeader.Add("subjectNR");
+            //savedData.csvHeader.Add("trialNR");
+            savedData.csvHeader.Add("isDecision");
+            savedData.csvHeader.Add("time");
+            savedData.csvHeader.Add("items");
+            savedData.csvHeader.Add("interactions");
+            savedData.csvHeader.Add("metres");
+            savedData.csvHeader.Add("failures");
+            //savedData.csvHeader.Add("levels");
+        }
     
-    void CreateCsvLine()
-    {
-        List<string> csvLine = new List<string>();
-
-        //csvLine.Add(savedData.subjectNr.ToString());
-        //csvLine.Add(savedData.trialNr.ToString());
-        
-        // "Yes" for experimental group, "No" for control group
-        if (savedData.isDecision)
+        void CreateCsvLine()
         {
-            csvLine.Add("Yes");
-        }
-        else
-        {
-            csvLine.Add("No");
-        }
-        
-        csvLine.Add(savedData.time.ToString(CultureInfo.InvariantCulture)); // unrounded data
+            List<string> csvLine = new List<string>();
 
-        csvLine.Add(savedData.items.ToString()); 
-        csvLine.Add(savedData.interactions.ToString());
-
-        csvLine.Add(savedData.metres.ToString(CultureInfo.InvariantCulture));
-        csvLine.Add(savedData.failures.ToString());
-        //csvLine.Add(savedData.levels.ToString());
+            //csvLine.Add(savedData.subjectNr.ToString());
+            //csvLine.Add(savedData.trialNr.ToString());
         
-        // add the csv line to csvData
-        savedData.csvData.Add(csvLine);
+            // "Yes" for experimental group, "No" for control group
+            if (savedData.isDecision)
+            {
+                csvLine.Add("Yes");
+            }
+            else
+            {
+                csvLine.Add("No");
+            }
+        
+            csvLine.Add(savedData.time.ToString(CultureInfo.InvariantCulture)); // unrounded data
+
+            csvLine.Add(savedData.items.ToString()); 
+            csvLine.Add(savedData.interactions.ToString());
+
+            csvLine.Add(savedData.metres.ToString(CultureInfo.InvariantCulture));
+            csvLine.Add(savedData.failures.ToString());
+            //csvLine.Add(savedData.levels.ToString());
+        
+            // add the csv line to csvData
+            savedData.csvData.Add(csvLine);
+        }
     }
 }
