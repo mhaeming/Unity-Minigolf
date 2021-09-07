e29d3bf2652dfce877b23908f6fb0813f1fe00c7
KrisKob <49274795+KrisKob@users.noreply.github.com>
2021-09-07 11:45:05 +0200

    added metres display

---
+++ Unity_Minigolf/Assets/Code/UI/Progress.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using Code.Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progress : MonoBehaviour
+{
+    private float _metresSoFar;
+    public Text metresText;
+
+    private void Start()
+    {
+        //
+    }
+
+    void Update()
+    {
+        _metresSoFar = PlayerInfo.DistanceTraveled;
+        DisplayMetres(_metresSoFar);
+        
+    }
+    
+    void DisplayMetres(float metresToDisplay)
+    {
+        float metres = Mathf.Round(metresToDisplay);
+        metresText.text = string.Format("{0} m", metres);
+    }
+}
